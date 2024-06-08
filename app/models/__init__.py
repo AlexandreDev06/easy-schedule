de7d7329b7d70f5ff6ec6f3665b6515623dfd181
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2024-06-08 17:45:02 -0300

    Creates base tables for show in demo

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,2 @@
 from .users import User
+from .messages import Message
--- app/models/users.py
+++ app/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, String
 from sqlalchemy.orm import relationship
 
 from app.configs.base import Base
@@ -9,7 +9,10 @@ class User(Base):
 
     __tablename__ = "users"
 
-    email = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
     password_digest = Column(String, nullable=True)
-
+    session_token = Column(String, nullable=True)
+    prompt = Column(String, nullable=True)
     name = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
+    messages = relationship("Message", back_populates="user", lazy="joined")
