dc1e5e44f1bdc0b8151d4495c45d38a63f589e5f
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2024-09-19 16:00:36 -0300

    Solved error to wrong serialization in login

--- app/controllers/auth/auth_controller.py
+++ app/controllers/auth/auth_controller.py
@@ -2,8 +2,7 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 import app.services.auth_services as auth_services
-from app.schemas.auth_schemas import OutputLoginSchema
-from app.schemas.user_schemas import UserSchema
+from app.schemas.auth_schemas import OutputLoginSchema, UserLoginSchema
 
 
 async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> OutputLoginSchema:
@@ -11,7 +10,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> OutputLogin
     user = await auth_services.user_exists(form_data.username)
 
     await auth_services.authenticate_user(form_data.username, form_data.password)
-    user_schema = UserSchema.model_validate(user)
+    user_schema = UserLoginSchema.model_validate(user.__dict__)
     access_token = await auth_services.create_access_token(user_schema)
 
     return OutputLoginSchema(
